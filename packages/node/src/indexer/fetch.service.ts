24a19aa797bc5e918a3a6c4baa8ea426add65b7d
Scott Twiname <skott.twiname@gmail.com>
2021-12-07 16:34:04 +1300

    Run prettier on fetch service

--- packages/node/src/indexer/fetch.service.ts
+++ packages/node/src/indexer/fetch.service.ts
@@ -32,10 +32,7 @@ import * as SubstrateUtil from '../utils/substrate';
 import { getYargsOption } from '../yargs';
 import { ApiService } from './api.service';
 import { AutoQueue } from './BlockedQueue';
-import {
-  Dictionary,
-  DictionaryService,
-} from './dictionary.service';
+import { Dictionary, DictionaryService } from './dictionary.service';
 import { DsProcessorService } from './ds-processor.service';
 import { IndexerEvent } from './events';
 import { BlockContent } from './types';
@@ -300,10 +297,11 @@ export class FetchService implements OnApplicationShutdown {
     this.latestProcessedHeight = height;
   }
 
-  private getScaledBatchSize = () => Math.max(
-    Math.round(this.batchSizeScale * this.nodeConfig.batchSize),
-    MINIMUM_BATCH_SIZE,
-  );
+  private getScaledBatchSize = () =>
+    Math.max(
+      Math.round(this.batchSizeScale * this.nodeConfig.batchSize),
+      MINIMUM_BATCH_SIZE,
+    );
 
   async startLoop(
     initBlockHeight: number,
@@ -468,7 +466,10 @@ export class FetchService implements OnApplicationShutdown {
     return false;
   }
 
-  private nextEndBlockHeight(startBlockHeight: number, scaledBatchSize: number): number {
+  private nextEndBlockHeight(
+    startBlockHeight: number,
+    scaledBatchSize: number,
+  ): number {
     const endBlockHeight = startBlockHeight + scaledBatchSize - 1;
     return Math.min(endBlockHeight, this.latestFinalizedHeight);
   }
