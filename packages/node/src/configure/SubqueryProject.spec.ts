b9694294d69ce8cf3aae0bd4efe2726abe6962da
bz888 <ziyu.ben158@gmail.com>
2022-06-20 16:39:20 +1200

    added manifest_v1 test

--- packages/node/src/configure/SubqueryProject.spec.ts
+++ packages/node/src/configure/SubqueryProject.spec.ts
@@ -9,6 +9,7 @@ describe('SubqueryProject', () => {
   describe('convert manifest to project object', () => {
     let projectDirV0_2_0: string;
     let projectDirV0_3_0: string;
+    let projectDirV1_0_0: string;
 
     beforeEach(() => {
       projectDirV0_2_0 = path.resolve(
@@ -20,6 +21,11 @@ describe('SubqueryProject', () => {
         __dirname,
         '../../test/projectFixture/v0.3.0',
       );
+
+      projectDirV1_0_0 = path.resolve(
+        __dirname,
+        '../../test/projectFixture/v1.0.0',
+      );
     });
 
     it('convert local 0.2.0 manifest to project object', async () => {
@@ -27,6 +33,7 @@ describe('SubqueryProject', () => {
       const project = await SubqueryProject.create(projectDirV0_2_0, {
         endpoint: 'wss://rpc.polkadot.io/public-ws',
       });
+
       expect((project.dataSources[1] as any).processor.file).toMatch(
         /moonbeam.js/,
       );
@@ -37,6 +44,7 @@ describe('SubqueryProject', () => {
       const project = await SubqueryProject.create(projectDirV0_3_0, {
         endpoint: 'wss://rpc.polkadot.io/public-ws',
       });
+
       expect((project.dataSources[1] as any).processor.file).toMatch(
         /moonbeam.js/,
       );
@@ -49,6 +57,33 @@ describe('SubqueryProject', () => {
         deployment,
         { endpoint: 'wss://rpc.polkadot.io/public-ws' },
         { ipfs: 'http://127.0.0.1:8080' },
+      );
+    }, 5000000);
+
+    it('convert 1.0.0 ipfs deployment to project object', async () => {
+      const expectedRunner = {
+        node: {
+          name: '@subql/node',
+          version: '>=1.0.0',
+        },
+        query: {
+          name: '@subql/query',
+          version: '*',
+        },
+      };
+
+      const project = await SubqueryProject.create(projectDirV1_0_0, {
+        endpoint: 'wss://rpc.polkadot.io/public-ws',
+      });
+      expect(project.runner).toMatchObject(expectedRunner);
+    }, 500000);
+
+    it('check processChainId', async () => {
+      const project = await SubqueryProject.create(projectDirV1_0_0, {
+        endpoint: 'wss://rpc.polkadot.io/public-ws',
+      });
+      expect(project.network.chainId).toMatch(
+        '0x401a1f9dca3da46f5c4091016c8a2f26dcea05865116b286f60f668207d1474b',
       );
     }, 5000000);
   });
